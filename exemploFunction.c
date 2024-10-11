4f32fd69a2fb9ee965ee929f42f0c96b76dc4d5c
Henrique Barbosa de Souza Júnior <henrique@hbveterinaria.com.br>
2024-10-11 20:12:06 -0300

    Exemplo 1

--- exemploFunction.c
+++ exemploFunction.c
@@ -2,11 +2,17 @@
 #include <stdlib.h>
 #include <locale.h>
 
+void  msg();
 
 int main() {
 	 setlocale(LC_ALL, "Portuguese_Brazil");
 	 
 
-	
+
+    msg();
 	return 0;
+}
+
+void  msg(){
+    printf("Sistema desenvolvido por \nHenrique Barbosa");
 }
