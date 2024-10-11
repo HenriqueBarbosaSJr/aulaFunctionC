bb3ab2c04eee23db9583dfad9016577cbdae4733
Henrique Barbosa de Souza Júnior <henrique@hbveterinaria.com.br>
2024-10-11 20:27:12 -0300

    Exemplo 2

--- exemploFunction.c
+++ exemploFunction.c
@@ -3,16 +3,32 @@
 #include <locale.h>
 
 void  msg();
+int  num();
+float media(float A, float B);
 
 int main() {
 	 setlocale(LC_ALL, "Portuguese_Brazil");
-	 
-
+	 int numero;
+     float result;
 
     msg();
+    numero = num();
+    printf("\n%i\n", numero);
+
+    result = media(5.6 , 8.7);
+    printf("\n%f\n", result);
+
 	return 0;
 }
 
 void  msg(){
-    printf("Sistema desenvolvido por \nHenrique Barbosa");
+    printf("Sistema desenvolvido por \nHenrique Barbosa\n");
+}
+
+int num(){
+    return 876;
+}
+
+float media(float A, float B){
+    return (A+B)/2;
 }
